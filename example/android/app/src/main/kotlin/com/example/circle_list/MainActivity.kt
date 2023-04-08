a966fd2be4296529551109fb20b9ec9919a18630
Aravindhan K <ark@tinisoft.in>
2023-04-08 13:09:56 +0530

    Chore: removing example, as got broke with our changes.

--- example/android/app/src/main/kotlin/com/example/circle_list/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.circle_list
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- example/lib/main.dart
+++
@@ -1,4 +0,0 @@
-import 'package:circle_list_demo/pages/demo_page.dart';
-import 'package:flutter/material.dart';
-
-void main() => runApp(MaterialApp(home: ShowDemoPage()));
